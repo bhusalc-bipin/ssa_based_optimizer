42f7f61d2cc2ac7adfc82a62f8c403df1a9792ff
Bipin Bhusal Chhetri <106178433+bhusalc-bipin@users.noreply.github.com>
2026-03-13 02:51:50 +0000

    Add test script

---
+++ test.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+./run.sh input/arrayparam.il
+./run.sh input/bubble.il
+./run.sh input/check.il
+./run.sh input/fib.il
+./run.sh input/gcd.il < input/gcd.in
+./run.sh input/while_array.il
